0663fd4c19953106970e7e8d53bf2ff141982064
achance2 <adchance2019@gmail.com>
2026-05-28 15:07:03 -0500

    Updated Website

--- aidan-portfolio/app/page.tsx
+++ aidan-portfolio/app/page.tsx
@@ -1,12 +1,121 @@
+"use client";
+
 import Navbar from "@/components/navbar";
-import Hero from "@/components/hero";
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { motion } from "motion/react";
+import { useRef } from "react";
 
 export default function Home() {
+  const constraintsRef = useRef(null);
   return (
-    <main className = "min-h-screen bg-slate-900 text-white">
+    <main>
       <Navbar />
-      <Hero />
+
+      <section className="relative h-screen overflow-hidden">
+        <iframe
+          src="https://my.spline.design/hana3dtextcopycopy-uIXXEcDpyB5yP5yCVGEs9BCI-dhb/"
+          className="absolute inset-0 h-full w-full border-0"
+          allow="fullscreen"
+        />
+
+        <div className="pointer-events-none absolute bottom-0 left-0 z-10 h-30 w-full bg-gradient-to-b from-transparent via-slate-900/60 to-slate-900" />
+
+      </section>
+
+      <section className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden text-center">
+        <div className="absolute inset-0 z-0 bg-[url('/images/home_images/background.jpg')] bg-cover bg-center" />
+        <div className="pointer-events-none absolute top-0 left-0 z-10 h-30 w-full bg-gradient-to-b from-slate-900 via-slate-900/60 to-transparent" />
+        <div className="absolute inset-0 z-0 bg-slate-900/40" />
+        <motion.nav
+          initial={{ y: -40, opacity: 0 }}
+          whileInView={{ y: -100, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 1.5, ease: "easeInOut" }}
+          className="flex flex-col items-center justify-center gap-4 bg-transparent px-4 py-4 text-white"
+        >
+          <Image
+            src="/images/home_images/Aidan.JPG"
+            alt="Aidan Photo"
+            width={400}
+            height={400}
+            className="rounded-full border-4 border-slate-900 object-cover"
+          />
+        </motion.nav>
+
+        <section className ="text-white">
+          <h1 className ="-translate-y-25 text-4xl font-bold text-white sm:text-5xl md:text-6xl">
+            Aidan Chance
+          </h1>
+          <h2 className ="-translate-y-30 text-xl font-semibold text-center">
+            ______________________________________
+          </h2>
+          <p className ="-translate-y-25 mx-auto max-w-3xl text-center">
+            Welcome to my website! My name is Aidan, and I am currently a Junior at Texas A&M University majoring in Interdisciplinary Engineering with a focus on Biomedical Engineering and Computer Science.
+          </p>
+        </section>
+
+        <section ref={constraintsRef} className="pointer-events-none absolute inset-0 z-20 overflow-hidden">
+          <motion.img
+            src="/images/home_images/python.svg"
+            alt="python"
+            width ={100}
+            height={100}
+            drag
+            dragMomentum={true}
+            dragElastic={0.8}
+            dragConstraints={constraintsRef}
+            className="pointer-events-auto absolute right-210 bottom-10 z-50 cursor-grab active:cursor-grabbing"
+          />
+
+          <motion.img
+            src="/images/home_images/C++.svg"
+            alt="C++"
+            width ={175}
+            height={175}
+            drag
+            dragMomentum={true}
+            dragElastic={0.8}
+            dragConstraints={constraintsRef}
+            className="pointer-events-auto absolute right-170 bottom-0 z-50 cursor-grab active:cursor-grabbing"
+          />
+
+          <motion.img
+            src="/images/home_images/CSS.svg"
+            alt="CSS"
+            width ={100}
+            height={100}
+            drag
+            dragMomentum={true}
+            dragElastic={0.8}
+            dragConstraints={constraintsRef}
+            className="pointer-events-auto absolute right-150 bottom-10 z-50 cursor-grab active:cursor-grabbing"
+          />
+
+          <motion.img
+            src="/images/home_images/javascript.svg"
+            alt="javascript"
+            width ={80}
+            height={80}
+            drag
+            dragMomentum={true}
+            dragElastic={0.8}
+            dragConstraints={constraintsRef}
+            className="pointer-events-auto absolute right-240 bottom-12.5 z-50 cursor-grab active:cursor-grabbing"
+          />
+
+          <motion.img
+            src="/images/home_images/typescript.svg"
+            alt="typescript"
+            width ={80}
+            height={80}
+            drag
+            dragMomentum={true}
+            dragElastic={0.8}
+            dragConstraints={constraintsRef}
+            className="pointer-events-auto absolute right-125 bottom-12.5 z-50 cursor-grab active:cursor-grabbing"
+          />
+        </section>
+      </section>
     </main>
   );
 }
--- aidan-portfolio/components/navbar.tsx
+++ aidan-portfolio/components/navbar.tsx
@@ -2,36 +2,77 @@
 
 import Link from "next/link";
 import { motion } from "motion/react";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <div className = "sticky top-0 z-50 flex justify-center p-6">
-        <motion.nav
-            initial = {{ y: -80, opacity: 0 }}
-            animate = {{ y: 0, opacity: 1 }}
-            transition = {{ duration: 0.6, ease: "easeInOut" }}
-            className="flex items-center justify-center gap-6 rounded-full border border-slate-800 bg-slate-950/80 px-8 py-4 text-white backdrop-blur-md"
-        >
-            <NavLink href="/">Home</NavLink>
-            <NavLink href="/projects">Projects</NavLink>
-            <NavLink href="/images/resume/Resume.pdf" target="_blank">Resume</NavLink>
-        </motion.nav>
+    <div className="absolute left-6 top-6 z-[9999]">
+      <motion.nav
+        initial={{ y: -60, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1.5, ease: "easeInOut" }}
+        className="flex flex-col items-start justify-center gap-4 bg-transparent px-4 py-4 text-white"
+      >
+        <NavLink href="/">Home</NavLink>
+        <NavLink href="/projects">Projects</NavLink>
+        <NavLink href="/images/resume/Resume.pdf" target="_blank">Resume</NavLink>
+
+        <Link href="https://github.com/achance2">
+          <Image
+            src="/images/home_images/github.svg"
+            alt="GitHub"
+            width={28}
+            height={28}
+            className = "invert"
+          />
+        </Link>
+
+        <Link href= "https://www.linkedin.com/in/aidan-chance">
+          <Image
+            src="/images/home_images/linkedin.svg"
+            alt="LinkedIn icon"
+            width={28}
+            height={28}
+            className = "invert"
+          />
+        </Link>
+
+        <Link href= "mailto:adchance2019@gmail.com">
+          <Image
+            src="/images/home_images/email.svg"
+            alt="email icon"
+            width={28}
+            height={28}
+            className = "invert"
+          />
+        </Link>
+
+        <Link href= "tel:+1-346-719-1543">
+          <Image
+            src="/images/home_images/phone.svg"
+            alt="phone icon"
+            width={28}
+            height={28}
+            className = "invert"
+          />
+        </Link>
+      </motion.nav>
     </div>
   );
 }
 
-function NavLink({ href, children, target }: { href: string; children: React.ReactNode; target?: string; }) {
-    return (
-        <motion.div whileHover={{ y: -2 }} whileTap={{ scale: 0.95 }}>
-        <Link
-            href = {href}
-            target = {target}
-            className="group relative text-sm text-slate-300 transition-colors duration-300 hover:text-white"
-        >
-            {children}
+function NavLink({href, children, target,}: {href: string; children: React.ReactNode; target?: string;}) {
+  return (
+    <motion.div whileHover={{ x: 10 }} whileTap={{ scale: 1.25 }} transition={{ type: "spring", stiffness: 400 }}>
+      <Link
+        href={href}
+        target={target}
+        className="group relative text-sm font-medium text-white transition-colors duration-300 hover:text-white-300"
+      >
+        {children}
 
-            <span className="absolute -bottom-1 left-0 h-[2px] w-0 bg-sky-400 transition-all duration-300 group-hover:w-full" />
-        </Link>
-        </motion.div>
-    );
+        <span className="absolute -bottom-1 left-0 h-[2px] w-0 bg-white-300 transition-all duration-300 group-hover:w-full" />
+      </Link>
+    </motion.div>
+  );
 }
